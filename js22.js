f4d7653fffec17eb12ffbbd9cb175a4cdb2cd536
youssef <youssefadamkhairy@gmail.com>
2018-04-19 06:07:05 +0200

    undo the modal
    many bugs when add when remove
    need to change whole code or use ajax

--- js22.js
+++ js22.js
@@ -1,61 +1,51 @@
 // element list
-var listv=document.getElementById("list");
+var listv=document.getElementById('list');
 // innn the list name input
-var innn= document.getElementById("input1"),
-colorid = document.getElementById('colorid');
+var innn= document.getElementById('input1'),
+	colorid = document.getElementById('colorid');
 
 var lstorage = JSON.parse(localStorage.getItem('list') ) ,
 	kokoj = lstorage;
 
 //add event listner on submit for add new list
-document.getElementById("NewList").addEventListener('submit', dataaa ) ;
+document.getElementById('NewList').addEventListener('submit', dataaa ) ;
 
 
 
-    //cheching if there Names or writes it's embty
-	if (lstorage==null || lstorage.listTitles.length==0){
+//cheching if there Names or writes it's embty
+if (lstorage==null || lstorage.listTitles.length==0){
 	var p= document.createElement('p');
-		p.textContent='The List Is Embty';
+	p.textContent='The List Is Embty';
 	listv.appendChild(p); 
-		kokoj={
+	kokoj={
 		listTitles:[],
 		listItems:[],
 		listColor:[]
-	}
+	};
 
-	} else {
+} else {
 	kokoj= JSON.parse(localStorage.getItem('list') ) ;
 	listn(lstorage.listTitles);
-	}
+}
 
-
-
-
-//changing background color
-// function color(){
-// 	colorid.setAttribute('value',lstorage.color);
-// 	kokoj.color=colorid.value;
-// 	localStorage.setItem('list', JSON.stringify(kokoj));
-// 	lstorage = JSON.parse(localStorage.getItem('list') ) ;
-// 	document.body.style.backgroundColor=lstorage.color;
-// }
 
 //function for event listner will be Ajax later
 function dataaa(e){
-    if (!innn.value==''){
+	if (!innn.value==''){
 		kokoj.listTitles.push(innn.value);
 		kokoj.listColor.push(colorid.value);
 
 		localStorage.setItem('list', JSON.stringify(kokoj));
 		lstorage = JSON.parse(localStorage.getItem('list') ) ;
-		//color();
-                        }
 
-        //color();
-                  }
+	}
+
+}
+
+
 
 // function to create list names depending on arry length
-function listn(arryname){
+function listn(arryname){ 
 	for (var i=0; i<arryname.length; i++){
 
 		var span= document.createElement('span');
@@ -69,78 +59,78 @@ function listn(arryname){
 
 		
 
-		var span3 = document.createElement("SPAN");
- 		var txt2 = document.createTextNode("\u00D7");
- 		span3.className = "close";
- 		span3.appendChild(txt2);
- 		pp.appendChild(span3);
+		var span3 = document.createElement('SPAN');
+		var txt2 = document.createTextNode('\u00D7');
+		span3.className = 'close';
+		span3.appendChild(txt2);
+		pp.appendChild(span3);
 
 
- 		span.setAttribute('class','listt');
- 		span.appendChild(colorls);
+		span.setAttribute('class','listt');
+		span.appendChild(colorls);
 		span.appendChild(pp);
 		span.appendChild(iinput);
 
 
 		//change color
-			colorls.addEventListener('change',(function(i){
+		colorls.addEventListener('change',(function(i){
 			return function(){
-			kokoj.listColor[i]=this.value;
-			localStorage.setItem('list',JSON.stringify(kokoj));
-			lstorage = JSON.parse(localStorage.getItem('list') ) ;
-			this.parentElement.style.backgroundColor=lstorage.listColor[i];
-		} } )(i) );
+				kokoj.listColor[i]=this.value;
+				localStorage.setItem('list',JSON.stringify(kokoj));
+				lstorage = JSON.parse(localStorage.getItem('list') ) ;
+				this.parentElement.style.backgroundColor=lstorage.listColor[i];
+			}; 
+		} )(i) );
 
 
 		// remove list
 		span3.onclick = (function(i) {
-		return function(){
-        kokoj.listTitles.splice(i,1);
-        kokoj.listItems.splice(i,1);
-        kokoj.listColor.splice(i,1);
+			return function(){
+				kokoj.listTitles.splice(i,1);
+				kokoj.listItems.splice(i,1);
+				kokoj.listColor.splice(i,1);
 
-    	localStorage.setItem('list', JSON.stringify(kokoj)) 
-        window.location.reload(); }
-    	 })(i)
+				localStorage.setItem('list', JSON.stringify(kokoj)) ;
+				window.location.reload(); 
+			};
+		} )(i) ;
 
-    	colorls.setAttribute('value',lstorage.listColor[i]);
-    	span.style.backgroundColor=lstorage.listColor[i];
+		colorls.setAttribute('value',lstorage.listColor[i]);
+		span.style.backgroundColor=lstorage.listColor[i];
 
 		listv.appendChild(span);
 
 
-
-
 		pp.onclick = (function(i) {
-		return function(){
-		var input3 = document.createElement("input");
-		input3.setAttribute('type','text');
-		input3.setAttribute('value',arryname[i]);
-		input3.setAttribute('class','style');
+			return function(){
+				var input3 = document.createElement('input');
+				input3.setAttribute('type','text');
+				input3.setAttribute('value',arryname[i]);
+				input3.setAttribute('class','style');
 
-		this.replaceWith(input3);
+				this.replaceWith(input3);
 
-		input3.addEventListener("keydown",function(e){
-			if(e.keyCode=== 13){ 
-				kokoj.listTitles[i]=input3.value;
-				localStorage.setItem('list', JSON.stringify(kokoj));
-				window.location.reload();
-			}
-		    })
-        }
-    	 }(i))
+				input3.addEventListener('keydown',function(e){
+					if(e.keyCode=== 13){ 
+						kokoj.listTitles[i]=input3.value;
+						localStorage.setItem('list', JSON.stringify(kokoj));
+						window.location.reload();
+					}
+		    });
+			};
+    	 }(i));
     	
     	
 
 
 		function itemInput(i){
 			return function(){
-			iinput.addEventListener('keydown', function(e){
-			if(e.keyCode==13){
-			var vvalue= e.target.value;
-			lstorage = JSON.parse(localStorage.getItem('list') ) ;
+				iinput.addEventListener('keydown', function(e){
+					if(e.keyCode==13){
+						var vvalue= e.target.value;
+						lstorage = JSON.parse(localStorage.getItem('list') ) ;
 
-			kokoj.listItems[i].push(vvalue);
+						kokoj.listItems[i].push(vvalue);
 
 
 		    localStorage.setItem('list', JSON.stringify(kokoj));
@@ -149,34 +139,32 @@ function listn(arryname){
   	        }
   	        }
 		  ); 
-			}()
+			}();
 		} itemInput(i) ;   
 
 
 
-		     itemss(span,i);
-
-
-    }   
+		itemss(span,i);
+	} 
 }
 
    
 
-		function itemss(parent,arryname){
+function itemss(parent,arryname){
 		    while(kokoj.listItems.length<kokoj.listTitles.length){
-			kokoj.listItems.push([]);
-			localStorage.setItem('list', JSON.stringify(kokoj));
-				break;
+		kokoj.listItems.push([]);
+		localStorage.setItem('list', JSON.stringify(kokoj));
+		break;
 		     } 
 
-		for (var s=0; s<kokoj.listItems[arryname].length ;s++){
+	for (var s=0; s<kokoj.listItems[arryname].length ;s++){
 		var ssh= document.createElement('p');
 		ssh.textContent=lstorage.listItems[arryname][s];
 		
 
-		var span2 = document.createElement("SPAN");
- 		var txt = document.createTextNode("\u00D7");
- 		span2.className = "close";
+		var span2 = document.createElement('SPAN');
+ 		var txt = document.createTextNode('\u00D7');
+ 		span2.className = 'close';
  		span2.appendChild(txt);
  		ssh.appendChild(span2);
 
@@ -184,7 +172,7 @@ function listn(arryname){
     	
 
     	function events(s){
-		return function(){
+			return function(){
 			
   	    ssh.addEventListener('click',function(e){
 
@@ -192,70 +180,70 @@ function listn(arryname){
 
 
      	case ('P') : 
-     	var input3 = document.createElement("input");
-		input3.setAttribute('type','text');
-		input3.setAttribute('value',kokoj.listItems[arryname][s]);
-		input3.setAttribute('class','style');
+     	var input3 = document.createElement('input');
+						input3.setAttribute('type','text');
+						input3.setAttribute('value',kokoj.listItems[arryname][s]);
+						input3.setAttribute('class','style');
 
-		this.replaceWith(input3);
+						this.replaceWith(input3);
 
-		input3.addEventListener("keydown",function(e){
-			if(e.keyCode=== 13){
-				kokoj.listItems[arryname][s]= e.target.value;
-				localStorage.setItem('list', JSON.stringify(kokoj));
-				window.location.reload();
-			}
-		})
+						input3.addEventListener('keydown',function(e){
+							if(e.keyCode=== 13){
+								kokoj.listItems[arryname][s]= e.target.value;
+								localStorage.setItem('list', JSON.stringify(kokoj));
+								window.location.reload();
+							}
+						});
      	 break ;
 
 
      	case('SPAN'): 
      	kokoj.listItems[arryname].splice(s,1);
-    	localStorage.setItem('list', JSON.stringify(kokoj)) 
-        window.location.reload(); 
+    	localStorage.setItem('list', JSON.stringify(kokoj)); 
+						window.location.reload(); 
      	 break ;
 
 
      	default:  
      	 break;
-        } 
-        });
+					} 
+				});
 
-		}() } events(s) ;
+			}(); } events(s) ;
 
 	
 		
 	    }
-		}
+}
 
 
 document.addEventListener('mouseover',function(e){
-			e.target.focus();
-})
+	e.target.focus();
+});
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
+// // Get the modal
+// var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById('myBtn');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+// 	modal.style.display = 'block';
+// };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+// 	modal.style.display = 'none';
+// };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+// 	if (event.target == modal) {
+// 		modal.style.display = 'none';
+// 	}
+// };
